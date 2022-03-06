77b1a7015a3e0a3cfc73b6338ee36a205ebddbfe
Josemi <jose@correo.com>
2022-03-06 20:34:56 +0100

    Creado main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+package javaapplication1;
+import java.util.Scanner;
+public class main {
+    
+      void mian(String[] args)
+        int numero;
+        Scanner leer = new Scanner(System.in));
+        
+        System.out.println("Esta es una aplicación de prueba.");
+        System.out.pintln("Trataremos de depurar el programa y observar...");
+        System.out.println("los errores más comunes en tiempo de compilación.");
+        
+        System.out.println("Introduce un número cualquiera:");
+        numero = leer.nextInt();
+        
+        if(numero > 0)
+            System.out.printf("El número %d introducido es positivo.",numero);
+        else{
+            out.printf("El número %d introducido es negativo.",numero);
+            System.out.println("¿Seguro, en todos los casos lo es...?");
+        
+        
+    }
+    
+}
+
